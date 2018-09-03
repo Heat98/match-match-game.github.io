b54f5696fd25cb45443400369faa6e16d3be7e9c
Heat98 <heeeat98@gmail.com>
2018-09-04 00:28:27 +0300

    Add cards front side

--- src/js/images.js
+++ src/js/images.js
@@ -3,18 +3,45 @@ const images = [
     {
         id: "naruto",
         images: [
+            './assets/img/naruto/1.png',
+            './assets/img/naruto/2.png',
+            './assets/img/naruto/3.png',
+            './assets/img/naruto/4.png',
+            './assets/img/naruto/5.png',
+            './assets/img/naruto/6.png',
+            './assets/img/naruto/7.png',
+            './assets/img/naruto/8.png',
+            './assets/img/naruto/9.png'
         ]
     },
 
     {
         id: "cats",
         images: [
+            './assets/img/cats/1.png',
+            './assets/img/cats/2.png',
+            './assets/img/cats/3.png',
+            './assets/img/cats/4.png',
+            './assets/img/cats/5.png',
+            './assets/img/cats/6.png',
+            './assets/img/cats/7.png',
+            './assets/img/cats/8.png',
+            './assets/img/cats/9.png'
         ]
     },
 
     {
         id: "web",
         images: [
+            './assets/img/web/1.png',
+            './assets/img/web/2.png',
+            './assets/img/web/3.png',
+            './assets/img/web/4.png',
+            './assets/img/web/5.png',
+            './assets/img/web/6.png',
+            './assets/img/web/7.png',
+            './assets/img/web/8.png',
+            './assets/img/web/9.png'
         ]
     }
 ];
