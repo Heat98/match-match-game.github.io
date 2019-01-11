236abe9da7996c5d376975cfeefffee52ce4fa53
Heat98 <heeeat98@gmail.com>
2019-01-11 22:55:42 +0300

    feat: change image path

--- src/js/images.js
+++ src/js/images.js
@@ -5,40 +5,40 @@ const images = [
         images: [
 
             {
-                src: '../src/assets/img/naruto/1.png',
+                src: 'src/assets/img/naruto/1.png',
                 id: 1
             },
             {
-                src: '../src/assets/img/naruto/2.png',
+                src: 'src/assets/img/naruto/2.png',
                 id: 2
             },
 
             {
-                src: '../src/assets/img/naruto/3.png',
+                src: 'src/assets/img/naruto/3.png',
                 id: 3
             },
             {
-                src: '../src/assets/img/naruto/4.png',
+                src: 'src/assets/img/naruto/4.png',
                 id: 4
             },
             {
-                src: '../src/assets/img/naruto/5.png',
+                src: 'src/assets/img/naruto/5.png',
                 id: 5
             },
             {
-                src: '../src/assets/img/naruto/6.png',
+                src: 'src/assets/img/naruto/6.png',
                 id: 6
             },
             {
-                src: '../src/assets/img/naruto/7.png',
+                src: 'src/assets/img/naruto/7.png',
                 id: 7
             },
             {
-                src: '../src/assets/img/naruto/8.png',
+                src: 'src/assets/img/naruto/8.png',
                 id: 8
             },
             {
-                src: '../src/assets/img/naruto/9.png',
+                src: 'src/assets/img/naruto/9.png',
                 id: 9
             },
         ]
@@ -49,39 +49,39 @@ const images = [
         images: [
 
             {
-                src: '../src/assets/img/cats/1.png',
+                src: 'src/assets/img/cats/1.png',
                 id: 1
             },
             {
-                src: '../src/assets/img/cats/2.png',
+                src: 'src/assets/img/cats/2.png',
                 id: 2
             },
             {
-                src: '../src/assets/img/cats/3.png',
+                src: 'src/assets/img/cats/3.png',
                 id: 3
             },
             {
-                src: '../src/assets/img/cats/4.png',
+                src: 'src/assets/img/cats/4.png',
                 id: 4
             },
             {
-                src: '../src/assets/img/cats/5.png',
+                src: 'src/assets/img/cats/5.png',
                 id: 5
             },
             {
-                src: '../src/assets/img/cats/6.png',
+                src: 'src/assets/img/cats/6.png',
                 id: 6
             },
             {
-                src: '../src/assets/img/cats/7.png',
+                src: 'src/assets/img/cats/7.png',
                 id: 7
             },
             {
-                src: '../src/assets/img/cats/8.png',
+                src: 'src/assets/img/cats/8.png',
                 id: 8
             },
             {
-                src: '../src/assets/img/cats/9.png',
+                src: 'src/assets/img/cats/9.png',
                 id: 9
             },
         ]
@@ -91,39 +91,39 @@ const images = [
         id: "web",
         images: [
             {
-                src: '../src/assets/img/web/1.png',
+                src: 'src/assets/img/web/1.png',
                 id: 1
             },
             {
-                src: '../src/assets/img/web/2.png',
+                src: 'src/assets/img/web/2.png',
                 id: 2
             },
             {
-                src: '../src/assets/img/web/3.png',
+                src: 'src/assets/img/web/3.png',
                 id: 3
             },
             {
-                src: '../src/assets/img/web/4.png',
+                src: 'src/assets/img/web/4.png',
                 id: 4
             },
             {
-                src: '../src/assets/img/web/5.png',
+                src: 'src/assets/img/web/5.png',
                 id: 5
             },
             {
-                src: '../src/assets/img/web/6.png',
+                src: 'src/assets/img/web/6.png',
                 id: 6
             },
             {
-                src: '../src/assets/img/web/7.png',
+                src: 'src/assets/img/web/7.png',
                 id: 7
             },
             {
-                src: '../src/assets/img/web/8.png',
+                src: 'src/assets/img/web/8.png',
                 id: 8
             },
             {
-                src: '../src/assets/img/web/9.png',
+                src: 'src/assets/img/web/9.png',
                 id: 9
             },
         ]
